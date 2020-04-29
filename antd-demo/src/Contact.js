74725f31bcb8f0aca789ec38f27abfe9e5ffb3e3
NickBoe <43213436+NickBoe@users.noreply.github.com>
2020-04-29 21:33:24 +0200

    Contacts draft first

--- antd-demo/src/Contact.js
+++ antd-demo/src/Contact.js
@@ -1,10 +1,23 @@
 import React from "react";
+import { Typography } from 'antd';
+import { Space, Card } from 'antd';
+
+const { Title } = Typography;
 
 function Contact() {
   return (
-    <div>
-      <h1> Contacts page </h1>
-    </div>
+    <Space direction="horizontal">
+    <Card title="Phone number" style={{ width: 300 }}>
+      <p>+45 XX XX XX XX</p>
+    </Card>
+    <Card title="Email" style={{ width: 300 }}>
+      <p>portfolio@gmail.com</p>
+    </Card>
+    <Card title="Social media and networking" style={{ width: 300 }}>
+      <p>Instagram</p>
+      <p>LinkedIn</p>
+    </Card>
+  </Space>
   );
 }
 
