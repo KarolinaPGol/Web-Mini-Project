ea6867305f58f8cfc5586a60cf40e9c221094718
Karolina Palica <31969537+KarolinaPGol@users.noreply.github.com>
2020-04-30 10:11:55 +0200

    minor color changes

--- antd-demo/src/NavigationBar.js
+++ antd-demo/src/NavigationBar.js
@@ -1,30 +1,62 @@
 import React from "react";
 import "./App.css";
-import { Link } from "react-router-dom";
-import { Row, Col } from "antd";
+import { NavLink } from "react-router-dom";
+import { Row, Col, Divider } from "antd";
+
+const DemoBox = (props) => (
+  <p className={`height-${props.value}`}>{props.children}</p>
+);
 
 function NavigationBar() {
   return (
     <div>
+      <Divider
+        orientation="left"
+        style={{ color: "#333", fontWeight: "normal" }}
+      ></Divider>
       <Row>
         <Col span={6}>
           <h1>
-            <Link to="/">Home</Link>
+            <NavLink
+              to="/"
+              style={{ color: "black" }}
+              activeStyle={{ color: "volcano-2" }}
+            >
+              Home
+            </NavLink>
           </h1>
         </Col>
         <Col span={6}>
           <h1>
-            <Link to="/portfolio">Portfolio</Link>
+            <NavLink
+              to="/portfolio"
+              style={{ color: "black" }}
+              activeStyle={{ color: "volcano-2" }}
+            >
+              Portfolio
+            </NavLink>
           </h1>
         </Col>
         <Col span={6}>
           <h1>
-            <Link to="/portfolio2">Portfolio2</Link>
+            <NavLink
+              to="/portfolio2"
+              style={{ color: "black" }}
+              activeStyle={{ color: "volcano-2" }}
+            >
+              Portfolio2
+            </NavLink>
           </h1>
         </Col>
         <Col span={6}>
           <h1>
-            <Link to="/contact">Contact</Link>
+            <NavLink
+              to="/contact"
+              style={{ color: "black" }}
+              activeStyle={{ color: "volcano-2" }}
+            >
+              Contact
+            </NavLink>
           </h1>
         </Col>
       </Row>
