68fb8a3d4c045d74f36bf178bcaf72b696958c6c
NickBoe <43213436+NickBoe@users.noreply.github.com>
2020-05-13 13:36:06 +0200

    Background image Contacts

--- antd-demo/src/Contact.js
+++ antd-demo/src/Contact.js
@@ -8,45 +8,54 @@ import phone from "./img/phoneIcon.jpg";
 import mail from "./img/mail.png";
 import instaIcon from "./img/instagram.png";
 import linkedinIcon from "./img/linkedin.png";
+import bckGround from "./img/prismatic.jpg";
 
 const { Title } = Typography;
 
 function Contact() {
   return (
-    <div style={{ background: '#ECECEC', padding: '200px' }}>
-    <Row gutter={16}>
-    <Col span={6}>
-    <Card>
-        <img alt="Phone" width="60%" src={phone} />
-        <h1>+45 XX XX XX XX</h1>
-    </Card>
-    </Col>
+      <div>
+      <div class="bgImage"></div>
+      <div class="cards">
+      <Row gutter={16} justify='center'>
+      <Col span={6}>
+      <Card>
+          <img alt="Phone" width="60%" src={phone}/>
+          <h1>+45 XX XX XX XX</h1>
+          
+      </Card>
+      </Col>
 
-    <Col span={6}>
-    <Card>
-        <img alt="Email" width="60%" src={mail} />
-        <h1>portfolio@gmail.com</h1>
-    </Card>
-    </Col>
+      <Col span={6}>
+      <Card>
+          <img alt="Email" width="60%" src={mail} />
+          <h1>portfolio@gmail.com</h1>
+      </Card>
+      </Col>
 
-    <Col span={6}>
-    <Card>
-        <a href="https://www.instagram.com/"> 
-        <img alt="Social media networking" width="60%" src={instaIcon} />
-        </a>
-        <div className="InstaText">
-        <h1><a href="https://www.instagram.com/"> Instagram account </a></h1>
-        </div>
-    </Card>
-    </Col>
-    <Col span={6}>
-    <Card>
-        <img alt="Social media networking" width="60%" src={linkedinIcon} />
-        <h1><a href="https://www.linkedin.com/"> LinkedIn account </a></h1>
-    </Card>
-    </Col>
-    </Row>
-    </div>
+      <Col span={6}>
+      <Card>
+          <a href="https://www.instagram.com/"> 
+          <img alt="Social media networking" width="60%" src={instaIcon} />
+          </a>
+          <div className="InstaText">
+          <h1><a href="https://www.instagram.com/"> Instagram account </a></h1>
+          </div>
+      </Card>
+      </Col>
+      <Col span={6}>
+      <Card>
+          <a href="https://www.linkedin.com/">
+          <img alt="Social media networking" width="60%" src={linkedinIcon} />
+          </a>
+          <div className="linkedInText">
+          <h1><a href="https://www.linkedin.com/"> LinkedIn account </a></h1>
+          </div>
+      </Card>
+      </Col>
+      </Row>
+      </div>
+      </div>
   );
 }
 
